f03dc0bba99f342bde09eb443df8c00258194381
Pandaen <sintpanda@gmail.com>
2013-03-21 17:34:03 +0100

    :)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JustBear::Application.config.secret_token = 'df097cd6917ed39e8c1c0bf7d7ce89779e7c4589745f5e3bca7c80568e4112b9e8b213db59740113c969fe8810a66380b92f0a6820dc9e5e8f185266f318b042'
